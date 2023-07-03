86cbb0f4822bceed62c1040292430393ef5edcef
Bonamente <56506035@protonmail.com>
2023-07-03 22:07:04 +0500

    feat: add Header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,20 @@
+import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import { HeaderProps } from './types';
+import styles from './header.module.scss';
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  return (
+    <AppBar position="static" className={styles.appBar}>
+      <Container className={styles.container}>
+        <Toolbar className={styles.toolbar}>
+          <div className={styles.cube} />
+          <Typography variant="h1" className={styles.title}>
+            {title}
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;

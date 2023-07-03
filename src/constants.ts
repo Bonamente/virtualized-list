db5a7030925501ba148992afe1353390b614553d
Bonamente <56506035@protonmail.com>
2023-07-03 22:08:01 +0500

    feat: add constants

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const ROW_TOP_GAP = 10;
+
+export const CELL_WIDTH = 262;
+export const CELL_HEIGHT = 350;
+export const CELL_TOP_GAP = 70;
+export const CELL_GAP = 30;

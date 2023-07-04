6a34a387eb765cc924553332b307511a08bbac60
Bonamente <56506035@protonmail.com>
2023-07-04 15:15:10 +0500

    feat: update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
+import Layout from './pages/layout/layout';
 import Home from './pages/home/home';
 import ProductPage from './pages/product-page/product-page';
 import NotFound from './pages/not-found/not-found';
@@ -7,8 +8,10 @@ import NotFound from './pages/not-found/not-found';
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="product/:id" element={<ProductPage />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
